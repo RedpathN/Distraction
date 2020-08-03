a1bda3ca241bbfa43bb6eea3b91bd14e3d2a6062
Nick Redpath <redpathn@gmail.com>
2020-08-03 13:57:58 +1200

    Camera

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject cameraTarget;
+    private Vector3 cameraOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraOffset = gameObject.transform.position - cameraTarget.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = cameraTarget.transform.position + cameraOffset;
+    }
+}
