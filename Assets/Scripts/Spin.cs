fed2bb3ad604e8d22b1d104fdd4033f80d1027be
Nick Redpath <redpathn@gmail.com>
2020-08-06 12:56:35 +1200

    TV

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float spinSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newRot = transform.rotation.eulerAngles;
+        newRot.y += spinSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(newRot);
+    }
+}
