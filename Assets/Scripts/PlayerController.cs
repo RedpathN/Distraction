2a8eda76c5c276d6e9165035cb2bdcfc8cd4e125
Nick Redpath <redpathn@gmail.com>
2020-08-06 23:53:56 +1200

    Physics Controller/Gameplay Tuning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -12,71 +13,128 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float turnSpeed;
     [SerializeField]
-    private float gravity;
+    private float maxFallTimer = 1;
+
+
+    private bool isMoving = false;
+    private bool isFalling = false;
+    private bool fallTimerOn = false;
+    private float fallTimer = 0;
 
     private Vector3 moveDir = Vector3.zero;
-
-    private bool falling = false;
+    private Rigidbody rb;
 
     private void Start()
     {
-        transform.position += new Vector3(0, nextHeight(), 0);
+        rb = GetComponent<Rigidbody>();
+        //transform.position += new Vector3(0, nextHeight().y, 0);
     }
     // Update is called once per frame
     void Update()
     {
         //Loc
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+        if ((Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) && !isFalling)
         {
-            moveDir = transform.forward;
+            isMoving = true;
         }
         else
         {
-            moveDir = Vector3.zero;
+            isMoving = false;
+        }
+
+        if (fallTimerOn)
+        {
+            fallTimer += Time.deltaTime;
+            bool falling = checkFalling();
+            if (!falling)
+            {
+                fallTimerOn = false;
+                fallTimer = 0;
+            }
+            else if (falling && fallTimer > maxFallTimer)
+            {
+                fallTimerOn = false;
+                fallTimer = 0;
+                isFalling = true;
+            }
+
         }
 
         transform.rotation = Quaternion.Euler(Vector3.up * (Input.GetAxis("Horizontal") * turnSpeed));
-
-        //Gravity
-        if (falling)
-        {
-            transform.position -= new Vector3(0, gravity, 0);
-        }
-
     }
 
     private void FixedUpdate()
     {
-        movePlayer();
-    }
+        if (isMoving)
+        {
+            movePlayer();
+        }
 
+        if (transform.position.y < -20f)
+        {
+            death();
+        }
+
+    }
     private void movePlayer()
     {
-        transform.position += ((transform.forward * Input.GetAxis("Vertical")) * (moveSpeed * Time.fixedDeltaTime)); 
-        transform.position += new Vector3(0, nextHeight(), 0);
+        rb.AddForce(((transform.forward * moveSpeed) + nextHeight()).normalized * Input.GetAxis("Vertical"), ForceMode.Impulse);
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, moveSpeed);
     }
-    private float nextHeight()
-    {
 
-        Vector3 origin = new Vector3(transform.position.x, transform.position.y, transform.position.z + (moveDir.z * Time.fixedDeltaTime*4));
+    private Vector3 nextHeight()
+    {
+        Vector3 origin = new Vector3(transform.position.x + 1.5f, transform.position.y, transform.position.z + 0.025f);
         RaycastHit hit;
-        float nextPos = 0;
+        Vector3 nextPos = Vector3.zero;
+
         LayerMask lm = LayerMask.GetMask("Walkable");
 
-        if (Physics.Raycast(origin, -transform.up, out hit, 1.5f, lm))
+        if (Physics.Raycast(origin, -transform.up, out hit, 3f, lm))
         {
-            falling = false;
             Debug.DrawRay(origin, -transform.up * hit.distance, Color.red);
-            nextPos = (hit.point.y - transform.position.y) + 1.01f;
+            nextPos = (hit.point - transform.position).normalized;
         }
-        else 
+        else
         {
-            falling = true;
+            fallTimerOn = true;
         }
 
         return nextPos;
     }
+    
+    private bool checkFalling()
+    {
+        bool falling = false;
 
+        float rad = GetComponent<CapsuleCollider>().radius * 0.75f;
+        List<Vector3> origins = new List<Vector3> { Vector3.zero, transform.forward, transform.right, -transform.forward, -transform.right };
+        foreach(Vector3 vec in origins)
+        {
+            Vector3 origin = transform.position + (vec * rad);
+            RaycastHit hit;
+
+            LayerMask lm = LayerMask.GetMask("Walkable");
+
+            if (Physics.Raycast(origin, -transform.up, out hit, 5f, lm))
+            {
+                falling = false;
+                Debug.DrawRay(origin, -transform.up * hit.distance, Color.red);
+            }
+            else
+            {
+                falling = true;
+            }
+            
+        }
+
+        return falling;
+    }
+
+    void death()
+    {
+
+    }
 
 
 }
