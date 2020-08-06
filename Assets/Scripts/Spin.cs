1a1920398e9a16eec3d4d26dbb1770e59b03dc2b
Nick Redpath <redpathn@gmail.com>
2020-08-06 15:09:51 +1200

    Controller

--- Assets/Scripts/Spin.cs
+++ Assets/Scripts/Spin.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Spin : MonoBehaviour
 {
-    public float spinSpeed;
+    public float spinSpeed = 15f;
 
     // Update is called once per frame
     void Update()
