e63714e016bd71360487c85eb73d2e34c60b6b65
Nick Redpath <redpathn@gmail.com>
2020-08-07 13:33:25 +1200

    StartScene

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Update is called once per frame
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+}
